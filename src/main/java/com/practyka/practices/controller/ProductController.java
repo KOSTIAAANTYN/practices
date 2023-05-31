a9d88471a00e862aa80842fe397c23fd592b7a12
K0STI@ <ksinogub333@gmail.com>
2023-05-31 11:36:37 +0200

    Add pictures

--- src/main/java/com/practyka/practices/controller/ProductController.java
+++ src/main/java/com/practyka/practices/controller/ProductController.java
@@ -8,19 +8,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
-    @GetMapping("/yuschenko")
-    public String products(Model model){
-        model.addAttribute("productList", productService.getProductList());
-        return "products";
-    }
+//    @GetMapping("/yuschenko")
+////    public String products(Model model){
+////        model.addAttribute("productList", productService.getProductList());
+////        return "products";
+////    }
     @GetMapping("/")
-    public String returnIndex(Model model){
-        model.addAttribute("productList", productService.getProductList());
+    public String returnIndex(@RequestParam(name = "title",required = false) String title, Model model){
+        model.addAttribute("productList", productService.getProductList(title));
         return "index";
     }
     @GetMapping("/product/{id}")
@@ -34,8 +36,11 @@ public class ProductController {
         return "redirect:/";
     }
     @PostMapping("/product/create")
-    public String createProduct(Product product){
-        productService.saveProduct(product);
+    public String createProduct(@RequestParam(name = "file1") MultipartFile file1,
+                                @RequestParam(name = "file2") MultipartFile file2,
+                                @RequestParam(name = "file3") MultipartFile file3,
+                                Product product){
+        productService.saveProduct(product,file1,file2,file3);
         return "redirect:/";
     }
 }
--- src/main/java/com/practyka/practices/services/ProductService.java
+++ src/main/java/com/practyka/practices/services/ProductService.java
@@ -1,11 +1,14 @@
 package com.practyka.practices.services;
 
+import com.practyka.practices.model.Image;
 import com.practyka.practices.model.Product;
 import com.practyka.practices.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,12 +17,50 @@ import java.util.List;
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public List<Product> getProductList() {
-        return productRepository.findAll();
+    public List<Product> getProductList(String title) {
+        if(title == null||title.equals(""))
+            return productRepository.findAll();
+        return productRepository.findByTitle(title);
     }
-    public void saveProduct(Product product){
+    public void saveProduct(Product product, MultipartFile file1,MultipartFile file2,MultipartFile file3){
+        Image image1;
+        Image image2;
+        Image image3;
+
+        if(file1.getSize()!=0){
+            image1 = toImageEntity(file1);
+            image1.setPreviewImage(true);
+            product.addImageToProduct(image1);
+        }
+        if(file2.getSize()!=0){
+            image2 = toImageEntity(file2);
+            product.addImageToProduct(image2);
+        }
+        if(file3.getSize()!=0){
+            image3 = toImageEntity(file3);
+            product.addImageToProduct(image3);
+        }
+        Product productFromDb=productRepository.save(product);
+
+        productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
+
         productRepository.save(product);
     }
+
+    private Image toImageEntity(MultipartFile file) {
+        Image image=new Image();
+        image.setName(file.getName());
+        image.setOriginalFileName(image.getOriginalFileName());
+        image.setContentType(file.getContentType());
+        image.setSize(file.getSize());
+        try {
+            image.setBytes(file.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return image;
+    }
+
     public void deleteProduct(Long id){
         productRepository.deleteById(id);
     }
