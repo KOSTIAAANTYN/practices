83d113f4af97c546ad92b18bcf0bdfc208e54ca4
K0STI@ <ksinogub333@gmail.com>
2023-06-05 20:07:22 +0200

    Bug fixed

--- src/main/java/com/practyka/practices/model/User.java
+++ src/main/java/com/practyka/practices/model/User.java
@@ -29,7 +29,7 @@ public class User implements UserDetails {
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "image_id")
     private Image avatar;
-    @Column(name = "numberPhone",unique = true)
+    @Column(name = "numberPhone")
     private String numberPhone;
     @Column(name = "active")
     private boolean active;
@@ -55,17 +55,17 @@ public class User implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
--- src/main/java/com/practyka/practices/repositories/UserRepository.java
+++ src/main/java/com/practyka/practices/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.practyka.practices.repositories;
 
 import com.practyka.practices.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface UserRepository extends JpaRepository<User,Long> {
     User findUserByEmail(String email);
